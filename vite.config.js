15d8b29559fd0ae631a344e881d17d243cac7a22
Matthew Button <hello@mattbutton.dev>
2023-11-01 16:35:03 +0000

    add: ENV Prefix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/movieland/"
+  base: "/movieland/",
+  build: {
+    envPrefix: 'VITE_REACT_'
+  }
 })
